524ddfbc5552bdf8bab38c8cfec3775aa18fbc16
Tim D. Smith <git@tim-smith.us>
2025-01-25 22:57:06 -0800

    day2.2

--- day02/day02.py
+++ day02/day02.py
@@ -6,11 +6,31 @@ import numpy as np
 def part1(records: list[np.ndarray]):
     is_ok = 0
     for record in records:
-        all_increasing = (record[1:] > record[:-1]).all()
-        all_decreasing = (record[1:] < record[:-1]).all()
-        deltas = np.abs(np.diff(record))
-        deltas_ok = ((deltas >= 1) & (deltas <= 3)).all()
-        is_ok += ((all_increasing | all_decreasing) & deltas_ok).all()
+        is_ok += is_safe(record)
+    return is_ok
+
+
+def is_safe(record: np.ndarray) -> bool:
+    all_increasing = (record[1:] > record[:-1]).all()
+    all_decreasing = (record[1:] < record[:-1]).all()
+    deltas = np.abs(np.diff(record))
+    deltas_ok = ((deltas >= 1) & (deltas <= 3)).all()
+    return bool(((all_increasing | all_decreasing) & deltas_ok).all())
+
+
+def is_safe_lossy(record: np.ndarray) -> bool:
+    if is_safe(record):
+        return True
+    for i in range(len(record)):
+        if is_safe(np.hstack((record[:i], record[i + 1 :]))):
+            return True
+    return False
+
+
+def part2(records: list[np.ndarray]):
+    is_ok = 0
+    for record in records:
+        is_ok += is_safe_lossy(record)
     return is_ok
 
 
@@ -19,6 +39,7 @@ def main(input: Path):
         np.array([int(x) for x in line.split()]) for line in input.open("rt") if line
     ]
     print(part1(records))
+    print(part2(records))
 
 
 if __name__ == "__main__":
